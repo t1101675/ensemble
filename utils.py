85ee5b16f2238783921a4a62986373fe7448d586
顾煜贤 <gu-yx17@mails.tsinghua.edu.cn>
2020-05-16 18:30:47 +0800

    tune

--- utils.py
+++ utils.py
@@ -127,13 +127,13 @@ def output_preds(preds, path):
 
 hyparam = {
     "threshold": 0,
-    "sample_rate": 0.2,
+    "sample_rate": 0.1,
     "title_sample_rate": 0.2,
-    "title_bagging_train_times": 60,
-    "bagging_train_times": 60,
+    "title_bagging_train_times": 200,
+    "bagging_train_times": 200,
     "boosting_train_times": 20,
     "max_features": 15000,
-    "title_max_features": 10000,
+    "title_max_features": 15000,
     "max_depth": 200,
     "tol": 1e-4,
     "C": 2,
